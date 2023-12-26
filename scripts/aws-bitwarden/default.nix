d18e2eb3020c78e960f3aa95f0b83ca3f4fcdde5
Gerard Ryan <gryan@localhost.localdomain>
2023-12-26 01:41:22 +0000

    📦 Wrap aws-bitwarden script

--- scripts/aws-bitwarden/default.nix
+++ scripts/aws-bitwarden/default.nix
@@ -1,12 +1,18 @@
-{ stdenv }:
+{ stdenv, lib, makeWrapper, awscli2, bitwarden-cli, jq }:
 
 stdenv.mkDerivation rec {
   name = "aws-bitwarden";
 
   src = ./.;
 
+  nativeBuildInputs = [ makeWrapper ];
+
   installPhase = ''
     mkdir -p $out/bin
     cp aws-bitwarden.sh $out/bin
+
+    # ensures versions of dependencies come from known locations
+    wrapProgram $out/bin/aws-bitwarden.sh \
+      --set PATH ${lib.makeBinPath [ awscli2 bitwarden-cli jq ]}
   '';
 }
