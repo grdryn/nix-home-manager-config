b9208f879d12f4b35c15cf6c8446e92ca236ff7d
Gerard Ryan <git@grdryn.xyz>
2024-01-12 18:26:11 +0000

    🚧 Add new machine

--- flake.nix
+++ flake.nix
@@ -54,6 +54,21 @@
         ];
       };
 
+      "grdryn@tower.desktop" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          xhmm.homeManagerModules.all
+          ./home.nix
+          ./shell.nix
+          ./emacs.nix
+          ./gnome.nix
+          ./git.nix
+          # Host Specific configs
+          ./tower.desktop/grdryn.nix
+        ];
+      };
+
     };
   };
 }
