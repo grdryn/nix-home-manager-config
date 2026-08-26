cfb142715acc2422283c171c50f1e4c7ee5935cd
Gerard Ryan <git@grdryn.xyz>
2026-08-26 15:54:56 +0100

    Install rosa cli

--- home.nix
+++ home.nix
@@ -123,6 +123,7 @@
     podlet
     poppler-utils
     prometheus.cli # promtool, see https://discourse.nixos.org/t/cannot-run-promtool-even-with-prometheus-installed-nixos-minimal/52064/3
+    rosa
     rtk
     ruff
     rustc
